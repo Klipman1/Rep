9a62215560863e35f1a9574ecf707ff7f2091ec3
Klipman1 <147398279+Klipman1@users.noreply.github.com>
2023-10-12 21:45:47 +0500

    Add files via upload

---
+++ Move.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public int speed;
+    public GameObject player;
+    public Transform transform;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+
+
+    void Update()
+    {
+        transform = (Input.GetAxis("Horizontal") * speed);
+    }
+}
